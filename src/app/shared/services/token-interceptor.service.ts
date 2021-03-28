149ee11fa1a9525ddffdc21007ed66fadc39ca74
DPostnik <48392818+DPostnik@users.noreply.github.com>
2021-03-28 11:30:52 +0300

    Update token-interceptor.service.ts

--- src/app/shared/services/token-interceptor.service.ts
+++ src/app/shared/services/token-interceptor.service.ts
@@ -1,21 +1,20 @@
-// import {Injectable} from '@angular/core';
-// import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-// import {Observable} from 'rxjs';
-// import {AuthService} from '../../admin/shared/services/auth.service';
-//
-// @Injectable()
-// export class TokenInterceptor implements HttpInterceptor {
-//
-//   constructor(public auth: AuthService) {}
-//
-//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-//
-//     request = request.clone({
-//       setHeaders: {
-//         Authorization: `Bearer_${this.auth.token}`
-//       }
-//     });
-//     console.log(`Bearer_${this.auth.token}`);
-//     return next.handle(request);
-//   }
-// }
+ import {Injectable} from '@angular/core';
+ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+ import {Observable} from 'rxjs';
+ import {AuthService} from '../../admin/shared/services/auth.service';
+
+ @Injectable()
+ export class TokenInterceptor implements HttpInterceptor {
+
+   constructor(public auth: AuthService) {}
+
+   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+     request = request.clone({
+       setHeaders: {
+         Authorization: `Bearer_${this.auth.token}`
+       }
+     });
+     console.log(`Bearer_${this.auth.token}`);
+     return next.handle(request);
+   }
+ }
